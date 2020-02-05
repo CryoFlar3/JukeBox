c7a5adadaa5cb77e735c358e80c40dc4187a48d3
Nyjel <nyjel.dukes@gmail.com>
2020-02-05 17:28:46 -0500

    Added a download button

--- app/src/main/java/org/computermentors/jukebox/MainActivity.java
+++ app/src/main/java/org/computermentors/jukebox/MainActivity.java
@@ -1,14 +1,43 @@
 package org.computermentors.jukebox;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadButton = findViewById(R.id.downloadButton);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
+                downloadSong();
+            }
+        });
+    }
+
+    private void downloadSong() {
+        long endTime = System.currentTimeMillis() + 10*1000;
+        while (System.currentTimeMillis() < endTime){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "Song downloaded!");
     }
 }
